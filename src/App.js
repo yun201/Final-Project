7435820118c87e78059ae856e285ac27083bf457
Yunqi Zhao <yunqizhao01@gmail.com>
2024-07-21 16:05:45 -0400

    Update deployment configuration

--- src/App.js
+++ src/App.js
@@ -1,34 +1,31 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './Home';
-import Games from './Games';
+
 import Contact from './Contact';
+import Games from './Games';
+import Home from './Home';
+import Navbar from './Navbar';
+import { Route, Routes } from 'react-router-dom';
 import GameInfo from './GameInfo';
 import NotFound from './NotFound';
 import CheckOut from './CheckOut';
-import Navbar from './Navbar';
-import Footer from './Footer';
 import { I18nextProvider } from 'react-i18next';
-import i18n from './i18n';
+import i18n from './i18n'; 
 
 function App() {
   return (
     <I18nextProvider i18n={i18n}>
-      <Router basename={process.env.PUBLIC_URL}>
-        <div className="App">
-          <Navbar />
-          <div className="content">
-            <Routes>
-              <Route exact path="/" element={<Home />} />
-              <Route path="/games" element={<Games />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/gameinfo/:id" element={<GameInfo />} />
-              <Route path="/checkout" element={<CheckOut />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </div>
-          <Footer />
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/games" element={<Games />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/gameinfo/:id" element={<GameInfo />} />
+            <Route path="/checkout" element={<CheckOut />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </div>
-      </Router>
+      </div>
     </I18nextProvider>
   );
 }
